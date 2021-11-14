e51bb927d17122e1c258447256d4a35ab334c0b8
shevchenkoAndriy <s.s.student.khai.edu>
2021-11-14 22:24:29 +0200

    add feature searchCountries

--- src/index.js
+++ src/index.js
@@ -2,56 +2,61 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'notiflix/dist/notiflix-3.2.2.min.css';
-import { fetchArticles } from './js/fetchCountries';
+import FetchCountries from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
-
+const fetchCountries = new FetchCountries();
 const searchBoxRef = document.querySelector('#search-box');
 const countryListRef = document.querySelector('.country-list');
 const countryInfoRef = document.querySelector('.country-info');
 searchBoxRef.addEventListener('input', debounce(onCheckInput, DEBOUNCE_DELAY));
 function onCheckInput(e) {
   const userInput = e.target.value.trim();
+  clearMarkup();
   if (userInput === '') {
-    clearMarkup();
     return;
   }
-  fetchArticles(userInput).then(verificationData).catchcatch(Notify.failure);
+  fetchCountries.query = e.target.value.trim();
+  fetchCountries.fetchCards().then(verificationData).catch(Notify.failure);
 }
 function verificationData(countries) {
   if (countries.length > 10) {
     return Notify.info('Too many matches found. Please enter a more specific name.');
-  } else if (countries.length === 0) {
-    return catchcatch(Notify.failure);
   } else if (countries.length >= 2 && countries.length <= 10) {
     return (countryListRef.innerHTML = countries.map(itemsMurcup).join(''));
   } else if (countries.length === 1) {
-    return cardMurcup(countries);
+    return cardMurcup(...countries);
+  } else {
+    Notify.failure('Oops, there is no country with that name');
   }
 }
 function itemsMurcup({ name, flags }) {
   return `
 <li class="country_item">
-   <img src="${flags.svg}" width="100px" alt="flag ${name.official}}">
+   <img class="country_flag" src="${flags.svg}" height="40px" width="50px" alt="flag ${name.official}}">
    <p>${name}</p>
 </li>
 `;
 }
 
-function cardMurcup(name, capital, population, flags, languages) {
-  return (countryListRef.innerHTML = `<article class='card animate__animated animate__fadeInUp' style="width: 18rem">
-  <img src="${flags.svg}" alt='flags ${name}' width="100px" class='card-img' />
-  <div class=''>"
-    <h2 class='card-title'>${name}}</h2>
-    <ul class=''>
-      <li class='list-group-item'>Population: ${population}</li>
-      <li class='list-group-item'>Languages: 
-      ${languages}
-      <li class='list-group-item'>Capital: ${capital}</li>
+function cardMurcup({ name, capital, population, flags, languages }) {
+  return (countryListRef.innerHTML = `
+  <div class="image-wrapper"><img src=${
+    flags.svg
+  } alt=flags ${name} width="50px" class='card-img' />
+  <h2 class='card-title'>${name}</h2></div>
+    <ul>
+      <li card__item>Population: ${population}</li>
+      <li card__item>Languages: 
+      ${languages.map(parseLanguages).join(', ')}
+      ${capital ? `<li card__item>Capital: ${capital}</li>` : ''}
     </ul>
-  </div>`);
+    `);
 }
 function clearMarkup() {
-  refs.countryListRef.innerHTML = '';
-  refs.countryInfoRef.innerHTML = '';
+  countryListRef.innerHTML = '';
+  countryInfoRef.innerHTML = '';
+}
+function parseLanguages({ name }) {
+  return `${name}`;
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,11 +1,24 @@
 const BASE_URL = 'https://restcountries.com/v2/name/';
 const FILTER_SETTINGS = '?fields=name,capital,population,flags,languages';
-export function fetchArticles(country) {
-  const url = `${BASE_URL}${country}${FILTER_SETTINGS}`;
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      return Promise.reject('Oops, there is no country with that name');
-    }
-    return response.json();
-  });
+export default class FetchCountries {
+  constructor() {
+    this.searchQuery = '';
+  }
+
+  fetchCards() {
+    const url = `${BASE_URL}${this.searchQuery}${FILTER_SETTINGS}`;
+
+    return fetch(url).then(response => {
+      console.log(response);
+      console.log('!response.ok', !response.ok);
+      if (!response.ok) {
+        return Promise.reject('Oops, there is no country with that name');
+      }
+      return response.json();
+    });
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
